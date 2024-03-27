c7320731a4f8bb67d78552f3d491ff3185d1c352
bhavana-netlify <bhavana@netlify.com>
2024-03-27 13:23:44 -0400

    adding an EF

--- edge-functions/blob.ts
+++ edge-functions/blob.ts
@@ -1,2 +1,11 @@
 import { getStore } from "@netlify/blobs";
 import type { Context } from "@netlify/edge-functions";
+
+
+export default async (req:Request, context: Context) => {
+    const Homes = getStore("Homes");
+    await Homes.set("place",  "Chennai", {metadata: {India: true}})
+    await Homes.set("place", "California", {metadata: {India: false}})
+
+    return new Response ("Values set in the Homes store")
+}
